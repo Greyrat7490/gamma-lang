28131e19e1fd00c8fb20276ca3e1962d87b753f5
Greyrat7490 <kevinschaperjahn13@gmail.com>
2022-01-29 16:14:27 +0100

    write syscall

--- gorec.go
+++ gorec.go
@@ -7,16 +7,15 @@ import (
     "os/exec"
     "unicode"
     "strings"
+    "strconv"
 )
-
-const SYS_WRITE = 1
-const SYS_EXIT = 60
 
 const STDOUT = 1
 
 type arg struct {
     isVar bool
-    regIdx int
+    argType gType
+    value int       // regIdx if isVar, strIdx if argType is str
 }
 
 type word struct {
@@ -110,9 +109,10 @@ func nasm_footer(asm *os.File) {
     asm.WriteString("\nmov rdi, 0\n")
     asm.WriteString(fmt.Sprintf("mov rax, %d\n", SYS_EXIT))
     asm.WriteString("syscall\n")
-    asm.WriteString("\nsection .data\n")
 
     if len(strLits) > 0 {
+        asm.WriteString("\nsection .data\n")
+
         for i, str := range strLits {
             asm.WriteString(fmt.Sprintf("str%d: db %s\n", i, str.value))
         }
@@ -123,21 +123,6 @@ func nasm_footer(asm *os.File) {
     asm.WriteString("intBuf:\n\tresb 12") // int(32bit) -> 10 digits max + \n and sign -> 12 char string max
 }
 
-func syscall(asm *os.File, syscallNum uint, args... interface{}) {
-    regs := []string{"rdi", "rsi", "rdx", "r10", "r8", "r9"}
-
-    if len(args) > len(regs) {
-        fmt.Fprintf(os.Stderr, "[ERROR] syscall only supports %d args\n", len(regs))
-        os.Exit(1)
-    }
-
-    for i, arg := range args {
-        asm.WriteString(fmt.Sprintf("mov %s, %s\n", regs[i], fmt.Sprint(arg)))
-    }
-
-    asm.WriteString(fmt.Sprintf("mov rax, %d\n", syscallNum))
-    asm.WriteString("syscall\n")
-}
 
 func getArgs(words []word, expectedArgCount int) (args []arg) {
     if len(words) < 2 || words[1].str != "(" {
@@ -151,12 +136,14 @@ func getArgs(words []word, expectedArgCount int) (args []arg) {
             break
         }
 
-        if isLit(w.str) {
-            args = append(args, arg{false, len(strLits)})
+        if w.str[0] == '"' && w.str[len(w.str) - 1] == '"' {
+            args = append(args, arg{false, str, len(strLits)})
             addStrLit(w)
+        } else if i, err := strconv.Atoi(w.str); err == nil {
+            args = append(args, arg{false, i32, i})
         } else {
             if v := getVar(w.str); v != nil {
-                args = append(args, arg{true, v.regIdx})
+                args = append(args, arg{true, v.vartype, v.regIdx})
             } else {
                 fmt.Fprintf(os.Stderr, "[ERROR] \"%s\" is not declared\n", w.str)
                 fmt.Fprintln(os.Stderr, "\t" + w.at())
@@ -177,50 +164,6 @@ func getArgs(words []word, expectedArgCount int) (args []arg) {
     }
 
     return args
-}
-
-
-// TODO: use stack to backup registers to prevent unwanted behavior
-func write(asm *os.File, words []word, i int) int {
-    args := getArgs(words[i:], 1)
-
-    if args[0].isVar {
-        v := vars[args[0].regIdx]
-        switch v.vartype {
-        case str:
-            if registers[v.regIdx].isAddr {
-                syscall(asm, SYS_WRITE, STDOUT, registers[v.regIdx].name, strLits[registers[v.regIdx].value].size)
-            } else {
-                fmt.Fprintln(os.Stderr, "[ERROR] unreachable: register.isAddr should always be true if type of var is String")
-                fmt.Fprintln(os.Stderr, "\t" + words[i].at())
-                os.Exit(1)
-            }
-
-        case i32:
-            if !registers[v.regIdx].isAddr {
-                asm.WriteString("push rbx\n")
-                asm.WriteString("push rax\n")
-                asm.WriteString(fmt.Sprintf("mov rax, %s\n", registers[v.regIdx].name))
-                asm.WriteString("call int_to_str\n")
-                syscall(asm, SYS_WRITE, STDOUT, "rbx", "rax")
-                asm.WriteString("pop rax\n")
-                asm.WriteString("pop rbx\n")
-            } else {
-                fmt.Fprintln(os.Stderr, "[ERROR] unreachable: register.isAddr should always be false if type of var is Int")
-                fmt.Fprintln(os.Stderr, "\t" + words[i].at())
-                os.Exit(1)
-            }
-
-        default:
-            fmt.Fprintf(os.Stderr, "[ERROR] unknown type \"%s\"\n", v.vartype.readable())
-            fmt.Fprintln(os.Stderr, "\t" + words[i].at())
-            os.Exit(1)
-        }
-    } else {
-        syscall(asm, SYS_WRITE, STDOUT, fmt.Sprintf("str%d", args[0].regIdx) , strLits[args[0].regIdx].size)
-    }
-
-    return i + len(args) + 2 // skip args, "(" and ")"
 }
 
 // escape chars (TODO: \n, \t, ...) (done: \\, \")
@@ -319,12 +262,12 @@ func compile(srcFile []byte) {
 
     for i := 0; i < len(words); i++ {
         switch words[i].str {
-        case "println":
-            i = write(asm, words, i)
         case "var":
             i = declareVar(words, i)
         case ":=":
             i = defineVar(asm, words, i)
+        case "println":
+            i = write(asm, words, i)
 
         default:
             fmt.Fprintf(os.Stderr, "[ERROR] keyword \"%s\" is not supported\n", words[i].str)
---
+++ syscalls.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+const SYS_WRITE = 1
+const SYS_EXIT = 60
+
+func syscall(asm *os.File, syscallNum uint, args... interface{}) {
+    regs := []string{"rdi", "rsi", "rdx", "r10", "r8", "r9"}
+
+    if len(args) > len(regs) {
+        fmt.Fprintf(os.Stderr, "[ERROR] syscall only supports %d args\n", len(regs))
+        os.Exit(1)
+    }
+
+    for i, arg := range args {
+        asm.WriteString(fmt.Sprintf("mov %s, %s\n", regs[i], fmt.Sprint(arg)))
+    }
+
+    asm.WriteString(fmt.Sprintf("mov rax, %d\n", syscallNum))
+    asm.WriteString("syscall\n")
+}
+
+// TODO: use stack to backup registers to prevent unwanted behavior
+func write(asm *os.File, words []word, i int) int {
+    args := getArgs(words[i:], 1)
+
+    if args[0].isVar {
+        v := vars[args[0].value]
+        switch v.vartype {
+        case str:
+            if registers[v.regIdx].isAddr {
+                syscall(asm, SYS_WRITE, STDOUT, registers[v.regIdx].name, strLits[registers[v.regIdx].value].size)
+            } else {
+                fmt.Fprintln(os.Stderr, "[ERROR] unreachable: register.isAddr should always be true if type of var is String")
+                fmt.Fprintln(os.Stderr, "\t" + words[i].at())
+                os.Exit(1)
+            }
+
+        case i32:
+            if !registers[v.regIdx].isAddr {
+                asm.WriteString("push rbx\n")
+                asm.WriteString("push rax\n")
+                asm.WriteString(fmt.Sprintf("mov rax, %s\n", registers[v.regIdx].name))
+                asm.WriteString("call int_to_str\n")
+                syscall(asm, SYS_WRITE, STDOUT, "rbx", "rax")
+                asm.WriteString("pop rax\n")
+                asm.WriteString("pop rbx\n")
+            } else {
+                fmt.Fprintln(os.Stderr, "[ERROR] unreachable: register.isAddr should always be false if type of var is Int")
+                fmt.Fprintln(os.Stderr, "\t" + words[i].at())
+                os.Exit(1)
+            }
+
+        default:
+            fmt.Fprintf(os.Stderr, "[ERROR] unknown type %d\n", v.vartype)
+            fmt.Fprintln(os.Stderr, "\t" + words[i].at())
+            os.Exit(1)
+        }
+    } else {
+        switch args[0].argType {
+        case str:
+            syscall(asm, SYS_WRITE, STDOUT, fmt.Sprintf("str%d", args[0].value) , strLits[args[0].value].size)
+        case i32:
+            asm.WriteString("push rbx\n")
+            asm.WriteString("push rax\n")
+            asm.WriteString(fmt.Sprintf("mov rax, %d\n", args[0].value))
+            asm.WriteString("call int_to_str\n")
+            syscall(asm, SYS_WRITE, STDOUT, "rbx", "rax")
+            asm.WriteString("pop rax\n")
+            asm.WriteString("pop rbx\n")
+        default:
+            fmt.Fprintf(os.Stderr, "[ERROR] unknown type %d\n", args[0].argType)
+            fmt.Fprintln(os.Stderr, "\t" + words[i].at())
+            os.Exit(1)
+        }
+    }
+
+    return i + len(args) + 2 // skip args, "(" and ")"
+}
