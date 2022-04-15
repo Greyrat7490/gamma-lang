a7b16172b6ca3055096e9b1d60f0fd00d7ba4d34
Greyrat7490 <kevinschaperjahn13@gmail.com>
2022-04-15 23:47:52 +0200

    removed OpType

--- ast/ast.go
+++ ast/ast.go
@@ -20,44 +20,13 @@ func Compile(asm *os.File) {
 }
 
 
-type OpType uint
-const (
-    OP_DEC_VAR  OpType = iota
-    OP_DEF_VAR
-    OP_DEF_FN
-    OP_CALL_FN
-    OP_BLOCK
-    OP_COUNT      uint = iota
-)
-
-func (o OpType) Readable() string {
-    // compile time reminder to add cases when Operants are added
-    const _ uint = 5 - OP_COUNT
-
-    switch o {
-    case OP_DEC_VAR:
-        return "OP_DEC_VAR"
-    case OP_DEF_VAR:
-        return "OP_DEF_VAR"
-    case OP_DEF_FN:
-        return "OP_DEF_FN"
-    case OP_BLOCK:
-        return "OP_BLOCK"
-    case OP_CALL_FN:
-        return "OP_CALL_FN"
-    default:
-        return ""
-    }
-}
-
-
 type Op interface {
     Readable(indent int) string
     Compile(asm *os.File)
 }
 
 type OpProgramm struct {
-    Ops []interface{ OpDecl } // only declaring/defining variables/functions allowed in global scope
+    Ops []OpDecl // only declaring/defining variables/functions allowed in global scope
 }
 
 func (o *OpProgramm) Readable(indent int) string {
